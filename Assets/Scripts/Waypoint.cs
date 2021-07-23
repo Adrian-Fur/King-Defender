f0049d6d860a7437d045c5e1999c2ef98f933ca0
Adrian Furmanek <adrian.furm@gmail.com>
2021-07-23 23:23:14 +0200

    Breadth First Search algoritm work in progress

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -10,12 +10,36 @@ public class Waypoint : MonoBehaviour
 
     public bool IsPlaceable { get { return isPlaceable; } }
 
+    GridManager gridManager;
+    PathFinding pathFinding;
+    Vector2Int coordinates = new Vector2Int();
+
+    private void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+        pathFinding = FindObjectOfType<PathFinding>();
+    }
+
+    private void Start()
+    {
+        if(gridManager != null)
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+
+            if (!isPlaceable)
+            {
+                gridManager.BlockNode(coordinates);
+            }
+        }    
+    }
+
     private void OnMouseDown()
     {
-        if (isPlaceable)
+        if (gridManager.GetNode(coordinates).isWalkable && !pathFinding.WillBlockPath(coordinates))
         {
             bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
             isPlaceable = !isPlaced;
+            gridManager.BlockNode(coordinates);
         }    
     }
 }
